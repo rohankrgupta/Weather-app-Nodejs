1504e898afae1dd4fc71db7f794110946c84eb6e
Rohan <guptarohan811@gmail.com>
2021-08-28 17:17:55 +0530

    Heroku deploy

--- public/js/app.js
+++ public/js/app.js
@@ -13,7 +13,7 @@ weatherForm.addEventListener('submit', (e)=>{
 
     const address = search.value;
     
-    fetch(`http://localhost:3000/weather?address=${address}`).then((res)=>{
+    fetch(`/weather?address=${address}`).then((res)=>{
     res.json().then((data) =>{
         if(data.error){
            return msgOne.textContent ='Invalid location';
